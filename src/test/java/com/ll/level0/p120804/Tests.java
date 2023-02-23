79db600170f4fac25aa8d2d0e2aa6ea64a2dec88
LIMLOSAS <gudwns3721@gmail.com>
2023-02-23 09:28:11 +0900

    ë 문제풀이 p120804í ã풀이

--- src/test/java/com/ll/level0/p120804/Tests.java
+++ src/test/java/com/ll/level0/p120804/Tests.java
@@ -19,12 +19,12 @@ public class Tests {
         assertThat(new Solution().solution(2, 3)).isEqualTo(-1);
     }
     @Test
-    @DisplayName("2, 3 =>1")
+    @DisplayName("11, 11 =>1")
     void t2 () {
         assertThat(new Solution().solution(11, 11)).isEqualTo(1);
     }
     @Test
-    @DisplayName("2, 3 =>-1")
+    @DisplayName("7,99 =>-1")
     void t3 () {
         assertThat(new Solution().solution(7, 99)).isEqualTo(-1);
         }
